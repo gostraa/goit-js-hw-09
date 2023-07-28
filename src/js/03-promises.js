053d006f5c2a045f75e2ec6cfa8aa3382dd92aac
gostraa <mcherkashina77@gmail.com>
2023-07-28 19:50:23 +0200

    Update 03-promises.js

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,8 +1,44 @@
-function createPromise(position, delay) {
-  const shouldResolve = Math.random() > 0.3;
-  if (shouldResolve) {
-    // Fulfill
-  } else {
-    // Reject
+
+import { Notify } from 'notiflix/build/notiflix-notify-aio';
+
+const formElem = document.querySelector(".form");
+  
+formElem.addEventListener("submit", onFormSubmit);
+
+
+function onFormSubmit(e) {
+  e.preventDefault();
+  const delay = Number(formElem.elements.delay.value);
+  const step = Number(formElem.elements.step.value);
+  const amount = Number(formElem.elements.amount.value);
+  
+  for (let index = 1; index <= amount; index += 1) {
+    createPromise(index, delay).then().catch();
+    delay += step;
+   
   }
+  
 }
+
+function createPromise(index, delay) {
+  const shouldResolve = Math.random() > 0.3;
+
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      if (shouldResolve) {
+        Notify.success(`✅ Fulfilled promise ${index} in ${delay}ms`)
+        resolve;
+      } else {
+        Notify.failure(`❌ Rejected promise ${index} in ${delay}ms`)
+        reject;
+      }
+    }, delay);
+ 
+  })
+}
+
+
+
+
+
+
